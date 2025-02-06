420adf462aefe7f13b0ad9acf361da00797b368a
smelly08 <toosmelly@outlook.com>
2025-02-06 09:22:16 -0500

    Update script.js

--- script.js
+++ script.js
@@ -9,10 +9,15 @@ document.getElementById('fetch-btn').addEventListener('click', async () => {
         const response = await fetch(`https://api.ashcon.app/mojang/v2/user/${username}`);
         const data = await response.json();
 
-        if (data && data.id) {
+        /*if (data && data.id) {
             document.getElementById('uuid-data').innerHTML = `
                 <h2>${username}'s UUID</h2>
                 <p>UUID: ${data.id}</p>
+            `;
+            */
+        if (data) {
+            document.getElementById('uuid-data').innerHTML = `
+                <h2>${data}</h2>
             `;
         } else {
             document.getElementById('uuid-data').innerHTML = "User not found.";
