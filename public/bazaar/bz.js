639e7c5962677fd36a55496aaf95c5e010f40187
smelly08 <toosmelly@outlook.com>
2025-02-14 12:05:26 -0500

    Update bz.js

--- public/bazaar/bz.js
+++ public/bazaar/bz.js
@@ -175,9 +175,11 @@ function displayItems(items) {
 // Function to filter items based on the search input
 function filterItems() {
     const searchTerm = document.getElementById('username').value.toLowerCase();
+    if (!globalItemList) {
+        updateBazaarData();
+    }
     if (searchTerm) {
         const filteredItems = globalItemList.filter(item => item.name.toLowerCase().includes(searchTerm));
-        alert(`${searchTerm}, ${filteredItems}`);
         displayItems(filteredItems); // Display filtered items
     } else {
         displayItems(globalItemList);
