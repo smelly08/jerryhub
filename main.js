03838954d4bd62ad1be8634466083252b25aed16
smelly08 <toosmelly@outlook.com>
2025-02-14 16:49:01 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-
+addEventListener("load", (event) => {
+    document.getElementById("main").innerHTML = `
+        <h1>JERRY'S HUB</h1>
+        <p>Welcome to Jerry's Hub, a Hypixel Skyblock utility website created by toosmelly.</p>
+        <button onclick="location.href='https://jerryhub.vercel.app/bazaar'" type="button">Bazaar Flipper</button>
+        <button onclick="location.href='https://jerryhub.vercel.app/profile'" type="button">Profile Viewer (not finished)</button>
+        <div id="stats"></div>
+    `;
+});
