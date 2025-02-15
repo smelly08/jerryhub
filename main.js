6d317b459644881bbfa76e795bcd0d44816bfae1
smelly08 <toosmelly@outlook.com>
2025-02-15 12:48:31 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 addEventListener("load", (event) => {
     document.getElementById("main").innerHTML = `
         <h1>JERRY'S HUB</h1>
-        <p>Welcome to <span class="purple">Jerry's Hub</span>, a Hypixel Skyblock utility website created by <span class="blue">toosmelly</span>.</p>
+        <p>Welcome to <span class="purple">Jerry's Hub</span>, a Hypixel Skyblock utility website created by <span class="darkaqua">toosmelly</span>.</p>
         <button onclick="location.href='https://jerryhub.vercel.app/bazaar'" type="button">Bazaar Flipper</button>
         <button onclick="location.href='https://jerryhub.vercel.app/profile'" type="button">Profile Viewer (not finished)</button>
         <div id="stats"></div>
