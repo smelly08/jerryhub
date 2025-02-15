fa3caf6774d3b093c9ab1c837231265e146e6274
smelly08 <toosmelly@outlook.com>
2025-02-15 12:34:19 -0500

    Update auction.js

--- public/ah/auction.js
+++ public/ah/auction.js
@@ -28,18 +28,9 @@ addEventListener("load", (event) => {
 // Add CSS for bz page
 const style = document.createElement('style');
 style.innerHTML = `
-    .invContainer {
-        margin: 20px;
-        padding: 100px;
-    }
     .ah-container {
         display: flex;
         justify-content: center;
-    }
-    #body {
-        display: flex;
-        justify-content: center;
-        align-items: center;
     }
 `;
 document.head.appendChild(style);
