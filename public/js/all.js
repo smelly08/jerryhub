3e6cea48ca6b18df7f38841c455276a0d270e797
smelly08 <toosmelly@outlook.com>
2025-02-15 18:09:56 -0500

    Update all.js

--- public/js/all.js
+++ public/js/all.js
@@ -3,12 +3,21 @@ addEventListener("load", (event) => {
     homeButton.innerHTML = `
         <img src="/public/img/jerryhead.jpeg" width="24px" height="24px" />
     `;
-    homeButton.style.position = "absolute";
-    homeButton.style.top = "0";
-    homeButton.style.left = "12px";
-    homeButton.style.padding = "20px";
-    homeButton.style.background = "#555555";
+    homeButton.setAttribute("id", "homeButton")
     document.body.appendChild(homeButton);
+    
+    const style = document.createElement("style");
+    style.innerHTML = `
+        #homeButton {
+            position: absolute;
+            top: 0;
+            left: 12px;
+            z-index: 9999;
+            padding: 4px;
+            background: #555555;
+        }
+    `;
+    document.head.appendChild(style);
     
     homeButton.addEventListener("click", (event) => {
         location.href= 'https://jerryhub.vercel.app';
