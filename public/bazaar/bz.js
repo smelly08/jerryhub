72903272f3bad2b62d8f6ab3db10a9ad1b00aa47
smelly08 <toosmelly@outlook.com>
2025-02-19 09:16:24 -0500

    Update bz.js

--- public/bazaar/bz.js
+++ public/bazaar/bz.js
@@ -188,8 +188,9 @@ function updateBazaarData(bazaarData, updated) {
 function displayItems(items) {
     let displayData = '<div class="grid-container">';
     items.forEach(item => {
-        let itemTexture = textures.item.name || "https://www.mc-heads.net/head/b6e522d918252149e6ede2edf3fe0f2c2c58fee6ac11cb88c617207218ae4595";
-        let itemName = textures.item.name || item.name;
+        let currentItemId = item.name
+        let itemTexture = textures.currentItemId.texture || "https://www.mc-heads.net/head/b6e522d918252149e6ede2edf3fe0f2c2c58fee6ac11cb88c617207218ae4595";
+        let itemName = textures.currentItemId.displayName || currentItemId;
         displayData += `
             <div class="grid-item">
                 <div class="inventory-slot">
