0ff0f5a6a42cf4f1e2ac2a33d223f6c29f9daf0c
smelly08 <toosmelly@outlook.com>
2025-02-28 13:48:21 -0500

    Update bz.js

--- public/bazaar/bz.js
+++ public/bazaar/bz.js
@@ -223,7 +223,7 @@ function changeSortMethod() {
         sortMethod = hourlyProfit;
         alert(`error, ${sortMethod} is not a valid sort method.`);
     }
-    updateBazaarData(globalItemList, lastUpdated);
+    displayItems(globalItemList.sort((a, b) => b[sortMethod] - a[sortMethod]));
 }
 
 fetchTexturesData();
